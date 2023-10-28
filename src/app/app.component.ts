550492163385c58c7d5bf7589cd37af0beef819d
JorgeTrinidadHerrera <jorge.trinidad@tecsup.edu.pe>
2023-10-27 22:05:54 -0500

    se agrega el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,40 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'appAngular';
+
+
+export class AppComponent{
+  nombre = 'Jorge Luis Trinidad Herrera';
+  edad = 23;
+  email = 'jorge.trinidad@tecsup.edu.pe';
+  sueldos = [1700, 1600, 1900];
+  activo = 'true'
+  sitio='http://www.google.com';
+  contador = 1;
+  profesion='';
+  anios='';
+
+  incrementar(){
+    this.contador++;
+  }
+
+  decrementar(){
+    this.contador--;
+  }
+
+  esActivo(){
+    if(this.activo)
+      return 'Trabajador Activo';
+    else
+      return 'Trabajador Inactivo';
+  }
+
+  ultimos3Sueldos(){
+    let suma = 0;
+    for(let x=0; x<this.sueldos.length; x++)
+      suma+=this.sueldos[x];
+      return suma;
+  }
+
+  
 }
